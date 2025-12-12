9fac9c5f5881304e9d5d155271e8b3b7010347b6
andrealangianese <andre.langia.01@gmail.com>
2025-12-12 16:01:28 +0100

    feat: esercizio finito

--- js/script.js
+++ js/script.js
@@ -23,6 +23,10 @@ const form = document.getElementById("answers-form");
 
 const okButton = document.querySelector(".btn-primary");
 
+//inserisco costante inputs
+
+const inputs = document.querySelectorAll("#input-group input");
+
 //----------------------------------------------__----------------------
 //creazione cronometro di 30 secondi
 
@@ -30,7 +34,7 @@ const okButton = document.querySelector(".btn-primary");
 
 let timer;
 
-let seconds = 3;
+let seconds = 10;
 
 //qua stiamo mostrando i 5 numeri dalla funzione che abbiamo creato precedentemente
 numberlist.innerText = (getArrRadnomNumInRangeTotEl(1, 50, 5));
@@ -59,11 +63,11 @@ timer = setInterval(() => {
         //document.getElementById("instructions").remove();
 
         //aggiungo stringa inserisci i numeri che ti ricordi
-//----------------------------------------------__----------------------
+        //----------------------------------------------__----------------------
 
         // metodo per sostituire un p con un altro testo
-        
-        let sostituzionep =document.getElementById("instructions");
+
+        let sostituzionep = document.getElementById("instructions");
 
         let sostitutop = document.createElement("p");
 
@@ -76,32 +80,52 @@ timer = setInterval(() => {
 }, 1000)
 //----------------------------------------------__----------------------
 
-
-
 //ELABORAZIONE
 
+const randomNumbers = getArrRadnomNumInRangeTotEl(1, 50, 5);
+numberlist.innerText = randomNumbers.join(", ")
 
 // gestione dell'evento di controllo dopo il click
 
-//controllo dei numeri corretti e feedback all'user
+form.addEventListener("submit", function (e) {
+
+    //lascio il tempo all'user di scrivere
+    e.preventDefault();
+
+    // creo array per inserire i suoi numeri e verificarli
+
+    let numberCorrect = [];
+
+    //faccio un ciclo sugli tutti gli input che mi da 
+    for (let i = 0; i < inputs.length; i++) {
+        // creo costante per trasformare l'input in numero e confrontarlo successivamente
+        const userNum = Number(inputs[i].value);
+        //controllo dei numeri
+        for (let x = 0; x < randomNumbers.length; x++) {
+            //se i numeri corrispondono li pusho nell'array vuoto
+            if (randomNumbers[x] === userNum) {
+                numberCorrect.push(userNum);
+
+            }
+            //return numberCorrect;
+        }
+    }
 
 
 
+    //controllo dei numeri corretti e feedback all'user
 
+    const popupMessage = document.getElementById("message");
 
-
-
-
-
-
-
+    popupMessage.innerText = `Hai preso ${numberCorrect.length} numeri : ${numberCorrect}`
+});
 
 
 
 //funzione per creare 5 numeri random non equivoci
 
 //debug in console
-console.log(getArrRadnomNumInRangeTotEl(1, 50, 5));
+//console.log(getArrRadnomNumInRangeTotEl(1, 50, 5));
 // funzione per creare un aray di tot numeri univoci
 
 function getArrRadnomNumInRangeTotEl(minNUm, maxNUm, tot) {
